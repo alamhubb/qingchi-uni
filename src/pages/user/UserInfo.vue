bc9e82131f325fe24a897a7ae89b9a11cc358bab
qinkaiyuan <qinkaiyuan94@163.com>
2020-07-16 21:13:07 +0800

    调整广告位置

--- src/pages/user/UserInfo.vue
+++ src/pages/user/UserInfo.vue
@@ -262,21 +262,21 @@
     </uni-popup>
 
     <talk-operate @deleteTalk="deleteTalk"></talk-operate>
+    <!--  #ifdef MP-WEIXIN -->
+    <ad v-if="talks.length>1" class="bg-white mb-5px w100vw" unit-id="adunit-65c8911d279d228f" ad-type="video"
+        ad-theme="white"></ad>
+    <!--  #endif -->
     <!--qq平台显示的广告-->
     <!--  #ifdef MP-QQ -->
     <ad v-if="talks.length>0" class="bg-white mb-5px w100vw" unit-id="72d8cb09a1bae9fa30d9e03e7cb8a25d"
         type="feeds" ad-height="160"></ad>
-    <!--  #endif -->
-    <!--  #ifdef MP-WEIXIN -->
-    <ad v-if="talks.length>1" class="bg-white mb-5px w100vw" unit-id="adunit-ffa7bc1c73c7d46a"></ad>
     <!--  #endif -->
     <view v-for="talk in talks" :key="talk.id">
       <talk-item :talk="talk" @deleteTalk="deleteTalk"></talk-item>
     </view>
     <!--wx平台显示的广告-->
     <!--  #ifdef MP-WEIXIN -->
-    <ad class="bg-white mt-10px w100vw" unit-id="adunit-65c8911d279d228f" ad-type="video"
-        ad-theme="white"></ad>
+    <ad class="bg-white mt-10px w100vw" unit-id="adunit-ffa7bc1c73c7d46a"></ad>
     <!--  #endif -->
     <!--qq平台显示的广告-->
     <!--  #ifdef MP-QQ -->
